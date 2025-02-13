eb564d0978fbdd16c6f1a1dd9ae4005fa3de2c98
Luiz Lima <devluizlima@gmail.com>
2025-02-13 11:54:40 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,20 +8,22 @@ import Login from './Pages/Login/Login'
 import Cadastro from './Pages/Cadastro/cadastro'
 import Principal from './Pages/Principal/Principal'
 
-function RotasPrivadas({ redirectTo }) {
-  const token = localStorage.getItem('token');
-  return token ? <Outlet /> : <Navigate to={redirectTo} />;
-}
+// API da escola de programação desligada!
+// function RotasPrivadas({ redirectTo }) {
+//   const token = localStorage.getItem('token');
+//   return token ? <Outlet /> : <Navigate to={redirectTo} />;
+// }
 
 export default function Rotas() {
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
+      // <Route path='/' element={<Login />} />
+      <Route path='/' element={<Principal />} />
       <Route path='/cadastro' element={<Cadastro />} />
 
-      <Route element={<RotasPrivadas redirectTo={'/'} />}>
-        <Route path='/principal/' element={<Principal />} />
-      </Route>
+      // <Route element={<RotasPrivadas redirectTo={'/'} />}>
+      //   <Route path='/principal/' element={<Principal />} />
+      // </Route>
     </Routes>
   )
 }
